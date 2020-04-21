a10cdf3a8823117cade760fc217a338da2a4b69a
babai10 <babai101@outlook.com>
2020-04-21 20:29:34 +0530

    New File Add

--- Project/src/main/java/Practice/App.java
+++ Project/src/main/java/Practice/App.java
@@ -14,7 +14,8 @@ public class App
         int c= a+b;
         
         System.out.println("The sum is = " + c);
-        
+        System.out.println("It is for git-----Finalchange------------ ");
+
         
     }
 }
