e98af42d1d184143b1af75c9d390ca6eb2e83376
babai10 <babai101@outlook.com>
2020-04-22 16:39:57 +0530

    New print add

---
+++ Project/src/main/java/Practice/App.java
@@ -0,0 +1,20 @@
+package Practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        int a =4;
+        int b = 6;
+        int c= a+b;
+        
+        System.out.println("The sum is = " + c);
+        System.out.println("There is a change in this branch");
+        
+    }
+}
